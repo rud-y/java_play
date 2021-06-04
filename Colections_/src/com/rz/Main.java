e01100d03d8a5186c2a6bf86bd0c32b4557c3e66
rud-y <rudolf.zachar@gmail.com>
2021-06-04 13:48:57 +0100

    Mapping class/map of teams and abbr

--- Colections_/src/com/rz/Main.java
+++ Colections_/src/com/rz/Main.java
@@ -2,23 +2,23 @@ package com.rz;
 
 import java.util.HashMap;
 import java.util.Map;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Map<String, String> nhlTeams = new HashMap<>();
-        nhlTeams.put("Montreal", "MTL");
-        nhlTeams.put("Toronto", "TOR");
-        nhlTeams.put("St. Louis","STL");
-        nhlTeams.put("New York Islanders", "NYI");
-
-        System.out.println(nhlTeams.get("Toronto"));
-
-        nhlTeams.remove("St. Louis");
-        System.out.println(nhlTeams.keySet());
-
-
-
-
-    }
-}
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Map<String, String> nhlTeams = new HashMap<>();
+//        nhlTeams.put("Montreal", "MTL");
+//        nhlTeams.put("Toronto", "TOR");
+//        nhlTeams.put("St. Louis","STL");
+//        nhlTeams.put("New York Islanders", "NYI");
+//
+//        System.out.println(nhlTeams.get("Toronto"));
+//
+//        nhlTeams.remove("St. Louis");
+//        System.out.println(nhlTeams.keySet());
+//
+//
+//
+//
+//    }
+//}
--- Colections_/src/com/rz/Mapping.java
+++ Colections_/src/com/rz/Mapping.java
@@ -1,9 +1,33 @@
 package com.rz;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
 
 public class Mapping {
 
+  public static void main(String[] args) {
+    Map<String, String> nhlTeams = new HashMap<>();
+    nhlTeams.put("Montreal", "MTL");
+    nhlTeams.put("Toronto", "TOR");
+    nhlTeams.put("St. Louis", "STL");
+    nhlTeams.put("New York Islanders", "NYI");
+    nhlTeams.put("Edmonton Oilers", "EDM");
+    nhlTeams.put("Phoenix", "PHX");
+    nhlTeams.put("Florida Panthers", "FLA");
+
+    System.out.println(nhlTeams.keySet());
+//    System.out.println("REMOVED: " +nhlTeams.remove("St. Louis"));
+    List<String> list = new ArrayList<>();
+    for (String key : nhlTeams.keySet()) {
+      if (key.contains(" ")) {
+        list.add(key);
+      }
+    }
+    System.out.println("Keys with more than one word: " + list);
+    List<String> sortedList = new ArrayList<String>(list);
+    Collections.sort(sortedList);
+    System.out.println("Sorted alphabetically: " +sortedList);
   }
+
+}
 
 
