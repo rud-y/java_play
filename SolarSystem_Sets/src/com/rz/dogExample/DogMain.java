d2824b0c34e56b215e1393f1c6ca8fe82e8e378a
rud-y <rudolf.zachar@gmail.com>
2021-07-08 11:03:39 +0100

    Equals() declared final (Dog class) and removed from any subclass!

--- SolarSystem_Sets/src/com/rz/dogExample/DogMain.java
+++ SolarSystem_Sets/src/com/rz/dogExample/DogMain.java
@@ -8,9 +8,14 @@ public class DogMain {
   public static void main(String[] args) {
 
     Labrador lab = new Labrador("Ashi");
+    Labrador lab2 = new Labrador("Ashi");
     Dog dog = new Dog("Ashi");
+
     System.out.println(lab.equals(dog));
     System.out.println(dog.equals(lab));
+
+    System.out.println(lab.getClass());
+    System.out.println(dog.getClass());
 
   }
 
--- SolarSystem_Sets/src/com/rz/dogExample/Labrador.java
+++ SolarSystem_Sets/src/com/rz/dogExample/Labrador.java
@@ -6,16 +6,17 @@ public class Labrador extends Dog {
     super(name);
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    if(this == obj){
-      return true;
-    }
-    if(obj instanceof Labrador){
-      String objName = ((Labrador) obj).getName();
-      return this.getName().equals(objName);
-    }
-    return false;
-  }
+  // Can't override equals/hashcode in Dog subclass (as Dog equals(..) is final
+//  @Override
+//  public boolean equals(Object obj) {
+//    if(this == obj){
+//      return true;
+//    }
+//    if(obj instanceof Labrador){
+//      String objName = ((Labrador) obj).getName();
+//      return this.getName().equals(objName);
+//    }
+//    return false;
+//  }
 
 }
